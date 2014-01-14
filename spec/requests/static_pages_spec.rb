dc33b99231ef65a0df63f4d24b9f252ed1a4500d
shariffh <shahuma2010@gmail.com>
2014-01-14 04:37:10 +0300

    Update tests for the Home pages title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,6 +26,15 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "About page" do
