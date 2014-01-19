5d26accf960815bc6a1bb50a7ffe0f3824005f22
shariffh <shahuma2010@gmail.com>
2014-01-19 16:04:25 +0300

    test for the links on the layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,5 +40,18 @@ describe "Static pages" do
     it { should have_title(full_title('Contact')) }
   end
 
-
+it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to # fill in
+    click_link "Contact"
+    expect(page).to # fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to # fill in
+    click_link "sample app"
+    expect(page).to # fill in
+  end
 end
