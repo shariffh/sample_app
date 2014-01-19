11c8635d3b4311c44caa4202d586d7b1912b5f29
shariffh <shahuma2010@gmail.com>
2014-01-19 16:03:01 +0300

    eliminate duplication

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,19 @@ describe "Static pages" do
 
   subject { page }
 
+   shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('Home')) }
-    #it { should_not have_title('| Home') }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
@@ -29,7 +36,9 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+
 end
