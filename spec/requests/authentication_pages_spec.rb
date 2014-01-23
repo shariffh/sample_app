2d76dad9d405fe6c4700bbac4541e7ca4cd940c0
shariffh <shahuma2010@gmail.com>
2014-01-23 18:42:38 +0300

    add code to test for signin failure

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "Authentication" do
+  describe "GET /authentication_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get authentication_pages_index_path
+      response.status.should be(200)
+    end
+  end
+describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
+    end
+  end
+  
+end
