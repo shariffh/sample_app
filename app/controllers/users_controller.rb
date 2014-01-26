f322199376a6ff77fa993520d02cc71bde7d9f76
shariffh <shahuma2010@gmail.com>
2014-01-26 12:49:41 +0300

    paginate the users in the index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,35 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+
 	def show
     @user = User.find(params[:id])
+  end
+  def index
+    @users = User.paginate(page: params[:page])
   end
   def new
   	 @user = User.new
   end
 
+def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+     flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
     	sign_in @user
         flash[:success] = "Welcome to the Sample App!"
     	redirect_to @user
-      # Handle a successful save.
+      
     else
       render 'new'
     end
@@ -23,5 +40,10 @@ private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
