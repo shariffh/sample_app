bc7210aa73cee56bb3050f1eb52dd8a833ca807f
shariffh <shahuma2010@gmail.com>
2014-01-30 20:55:37 +0300

    Add the user model followed_user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-	before_save { self.email = email.downcase }
+	has_many :followed_users, through: :relationships, source: :followed
+  before_save { self.email = email.downcase }
 	before_create :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
