5d60165a43b96e1b7c623d5632f309bfcde7a0e8
shariffh <shahuma2010@gmail.com>
2014-02-03 15:54:28 +0300

    add some changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  subject { page }
+subject { page }
 
-   shared_examples_for "all static pages" do
+shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }
     it { should have_title(full_title(page_title)) }
   end
 
   describe "Home page" do
     before { visit root_path }
-
-    let(:heading)    { 'Sample App' }
+    let(:heading) { 'Sample App' }
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
-    #it { should_not have_title('Home') }
+    it { should_not have_title('| Home') }
+
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -32,6 +32,7 @@ describe "Static pages" do
         end
       end
 
+      
       describe "follower/following counts" do
         let(:other_user) { FactoryGirl.create(:user) }
         before do
@@ -43,31 +44,14 @@ describe "Static pages" do
         it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
-  end
-
-    describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        sign_in user
-        visit root_path
-      end
-
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          expect(page).to have_selector("li##{item.id}", text: item.content)
-        end
-      end
-    end
-  end
+end
 
   describe "Help page" do
     before { visit help_path }
 
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
-  end
+end
 
   describe "About page" do
     before { visit about_path }
@@ -82,6 +66,4 @@ describe "Static pages" do
     it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
-
-
 end
