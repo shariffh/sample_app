998d5377a950911e8df0f218f814b99bc867c13e
shariffh <shahuma2010@gmail.com>
2014-02-13 14:25:41 +0000

    Add precompiled assets for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140129084852) do
+ActiveRecord::Schema.define(version: 20140204111145) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -40,7 +40,10 @@ ActiveRecord::Schema.define(version: 20140129084852) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",                  default: false
+    t.string   "auth_token"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
